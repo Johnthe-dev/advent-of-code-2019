2bc2aed5972427caf64c96b9c2c4be1fa4166b77
John Johnson-Rodgers <john@johnthe.dev>
2019-12-02 08:40:54 -0700

    set up if structure for day two challenge 1

---
+++ day-2/challenge-1.js
@@ -0,0 +1,18 @@
+//First challenge of the Advent of Code 2019.
+//Problem may be found at https://adventofcode.com/2019/day/2
+//solution by John Johnson-Rodgers <john@johnthe.dev>
+
+programNumbers = [];
+let i = 0;
+while(i<programNumbers.length) {
+	if (programNumbers[i]===1) {
+
+	} else if (programNumbers[i]===2) {
+
+	} else if (programNumbers[i]===99) {
+
+	} else {
+		console.log("ERROR ERROR");
+		i = programNumbers.length + 1;
+	}
+}
