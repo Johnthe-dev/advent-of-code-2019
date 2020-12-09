c36d441650ad3afc541642662ce96ad12461adbd
John Johnson-Rodgers <john@johnthe.dev>
2020-12-09 07:42:26 -0700

    finished part one of day 9, working on part two, looks promising

--- day-9-2/day-9.js
+++ day-9-2/day-9.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 let input = fs.readFileSync('./input.txt', {encoding: 'utf8', flag: 'r'});
 let lines = input.split('\n');
-console.log(lines);
 let numOfPrev = 25;
-let twentyFivePrev = lines.slice(0, numOfPrev-1);
+let twentyFivePrev = lines.slice(0, numOfPrev);
 let broken=false;
 let i=numOfPrev;
 while (broken!==true){
@@ -19,6 +18,37 @@ while (broken!==true){
  }
  twentyFivePrev.push(lines[i]);
  twentyFivePrev.shift();
+ console.log(twentyFivePrev);
  i++;
 }
-console.log(lines[i]);
+let breakpoint = parseInt(lines[i-1], 10);
+let summed = false;
+let start = 0;
+while (!summed){
+    let under = true;
+    while(under){
+        let sum = 0
+        for(let j =start; j<lines.length; j++){
+            sum=sum+parseInt(lines[j], 10);
+            if(sum>breakpoint){
+                j = lines.length;
+                under=false;
+            } else if(sum===breakpoint){
+                let smallest = 1000000000000;
+                let largest = 0;
+                for(i=start; i>j; i++){
+                    if(parseInt(lines[i], 10)<smallest){
+                        smallest=parseInt(lines[i], 10);
+                    }
+                    if(parseInt(lines[i], 10)>largest){
+                        largest = parseInt(lines[i], 10);
+                    }
+                    console.log(smallest+' '+largest);
+                }
+                summed=true;
+            }
+        }
+    }
+    start++;
+
+}
